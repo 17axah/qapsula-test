ec477ff1debf3dd792d77b4781200a85ea0fa067
17axaH <17axaH@mail.ru>
2021-01-26 16:59:43 +0000

    layout components

---
+++ components/layout/PageLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="page-layout">
+    <slot name="header" />
+    <div class="page-layout__inner">
+      <slot name="body" />
+    </div>
+    <slot name="footer" />
+  </div>
+</template>
+
+<style lang="sass" scoped>
+.page-layout
+  display: flex
+  min-height: 100vh
+  flex-direction: column
+
+  &__inner
+    flex: 1 0 auto
+</style>
---
+++ components/layout/Wrapper.vue
@@ -0,0 +1,36 @@
+<template>
+  <component :is="tag" class="wrapper" :style="styles">
+    <slot />
+  </component>
+</template>
+
+<script>
+export default {
+  props: {
+    tag: {
+      type: String,
+      default: 'div',
+    },
+    width: {
+      type: String,
+      default: '1440px',
+    },
+  },
+  computed: {
+    styles() {
+      return {
+        'max-width': this.width,
+      }
+    },
+  },
+}
+</script>
+
+<style lang="sass" scoped>
+.wrapper
+  margin-left: auto
+  margin-right: auto
+  padding-left: 24px
+  padding-right: 24px
+  box-sizing: content-box
+</style>
