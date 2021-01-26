db609ee34c738ae8990e619cdc8e71b2178942e3
17axaH <17axaH@mail.ru>
2021-01-26 19:00:51 +0000

    public layout

---
+++ layouts/public.vue
@@ -0,0 +1,29 @@
+<template>
+  <page-layout>
+    <template #header>
+      <public-header />
+    </template>
+    <template #body>
+      <main>
+        <nuxt />
+      </main>
+    </template>
+    <template #footer>
+      <public-footer />
+    </template>
+  </page-layout>
+</template>
+
+<script>
+import PageLayout from '@/components/layout/PageLayout.vue'
+import PublicHeader from '@/components/layout/PublicHeader.vue'
+import PublicFooter from '@/components/layout/PublicFooter.vue'
+
+export default {
+  components: {
+    PageLayout,
+    PublicHeader,
+    PublicFooter,
+  },
+}
+</script>
